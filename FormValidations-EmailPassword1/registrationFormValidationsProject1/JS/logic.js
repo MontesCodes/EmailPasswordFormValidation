cae51b3d20ee8936121356c166bca6c1ac5a80ae
MontesCodes <MontiesWebDev@Gmail.com>
2024-04-13 13:31:58 -0400

    stopped vid 3 Time 2:45 JS 4/13/2024

--- FormValidations-EmailPassword1/registrationFormValidationsProject1/JS/logic.js
+++ FormValidations-EmailPassword1/registrationFormValidationsProject1/JS/logic.js
@@ -3,6 +3,9 @@ const logInLink = document.querySelector('#login-link');
 const signUpFormRepository = document.querySelector('.form-repository__signup');
 const logInFormRepository = document.querySelector('.form-repository__login');
 
+const signupForm = document.querySelector('.signup-form');
+const loginForm = document.querySelector('.login-form');
+
 signUpLink.addEventListener('click', () => {
   signUpFormRepository.classList.remove('hide');
   logInFormRepository.classList.add('hide');
