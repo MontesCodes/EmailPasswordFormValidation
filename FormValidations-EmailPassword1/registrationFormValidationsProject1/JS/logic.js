1bd97154474768e2c7020879fc8013889170ae93
MontesCodes <MontiesWebDev@Gmail.com>
2024-04-17 21:02:35 -0400

    finished Vid 3 JS 4/17/2024

--- FormValidations-EmailPassword1/registrationFormValidationsProject1/JS/logic.js
+++ FormValidations-EmailPassword1/registrationFormValidationsProject1/JS/logic.js
@@ -36,23 +36,43 @@ signupForm.addEventListener('submit', events => {
   const password = signupForm.querySelector('#signup-password');
   const confirmPassword = signupForm.querySelector('#signup-confirm-password');
 
+  // checks if email is empty and displays message if email is empty
   if (!email.value.trim()) flashMessage(email, 'email is really required!!');
 
+  // checks if email is invalid and displays message if email is invalid
   const isEmailValid = validateEmail2(email.value.trim());
   if (!isEmailValid && email.value.trim())
     flashMessage(email, 'Hey! invalid email!!');
 
+  // checks if email is valid and displays message if email is valid
+  const isEmailValid2 = validateEmail2(email.value.trim());
+  if (isEmailValid2 && email.value.trim())
+    flashMessage(email, 'Looking good! email is valid!!');
+
+  // checks if password is empty and displays message if password is empty
   if (!password.value.trim())
     flashMessage(password, 'Hey! password is empty!!');
 
+  // checks if password is less than 10 characters and displays message if password is less than 10 characters
   if (
     !password.value.trim().length < 10 &&
     email.value.trim() &&
     password.value.trim()
-  )
+  ) {
     flashMessage(password, 'Hey! password must be at least 10 characters!!');
+  }
+  // checks if confirm password matches password and checks if confirm password is empty and displays message if confirm password does not match
+  if (
+    password.value.trim() !== confirmPassword.value.trim() &&
+    confirmPassword.value.trim()
+  ) {
+    flashMessage(confirmPassword, 'Hey! confirm password does not match!!');
+  }
+  // checks if confirm password is empty and displays message if confirm password is empty
+  if (!confirmPassword.value.trim())
+    flashMessage(confirmPassword, 'Hey! confirm password is empty!!');
 });
-
+// flash message for closet div
 const flashMessage = (element, message) => {
   const msg = element.closest('div').previousElementSibling;
   msg.textContent = message;
@@ -62,7 +82,7 @@ const flashMessage = (element, message) => {
     msg.style.visibility = 'hidden';
   }, 3000);
 };
-
+// checks if email is valid using regex
 const validateEmail2 = checkEmail2 => {
   var re = /\S+@\S+\.\S+/;
   return re.test(String(checkEmail2).toLowerCase());
